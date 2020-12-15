3c0faf2fcf876092be27026e5a1dea3fb3ab5a33
Andrew Hettinger <Andrew Hettinger@prominic-ad.com>
2020-12-14 22:30:43 -0600

    Add drawio

---
+++ automatic/drawio/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$drawioversion = '13.9.9'
+$url        = "https://github.com/jgraph/drawio-desktop/releases/download/v$drawioversion/draw.io-$drawioversion-windows-installer.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  #file          = "$toolsDir/draw.io-11.1.4-windows-installer.exe"
+  url           = $url
+  softwareName  = 'drawio*'
+  checksum      = '00b08fda74d68c62867de01059f45a246a8294e534f36b40b6b5602bf2371ebebe800aa0260625a3ae7922a8c8e3289ba683c0a5d2db581e39c591e028e192ae'
+  checksumType  = 'sha512'
+  silentArgs   = '/S'
+}
+
+#If ([bool](get-command Get-ChecksumValid -ea silentlycontinue))
+#{
+#  Get-ChecksumValid -File $($packageArgs.file) -checksumType $($packageArgs.checksumType) -checksum $($packageArgs.checksum)
+#}
+
+Install-ChocolateyPackage @packageArgs
+
+#Create .ignore files so chocolatey does not shim the Exe
+$files = get-childitem $toolsDir -include *.exe -recurse
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
