2136bfe6ce702e3456332416962843ed7a65637c
Andrew Hettinger <Andrew Hettinger@prominic-ad.com>
2020-12-14 21:33:01 -0600

    Add cmder
    Note: this is a portable package.

---
+++ automatic/cmder/update.ps1
@@ -0,0 +1,56 @@
+﻿Import-Module AU
+
+$repoInfo = @{
+  repoUser = 'cmderdev'
+  repoName = 'cmder'
+}
+
+function global:au_BeforeUpdate($Package) {
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { rm -Force $licenseFile }
+
+  iwr -UseBasicParsing -Uri $($Package.nuspecXml.package.metadata.licenseUrl -replace 'blob','raw') -OutFile $licenseFile
+  if (!(Get-ValidOpenSourceLicense -path "$licenseFile")) {
+    throw "Unknown license download. Please verify it still contains distribution rights."
+  }
+
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUrl)>"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+  }
+}
+
+function global:au_AfterUpdate {
+  Update-Metadata -key 'releaseNotes' -value "
+[Package changelog](https://github.com/AdmiringWorm/chocolatey-packages/blob/master/automatic/cmder/Changelog.md)
+
+### Software changelog
+$($Latest.ReleaseNotes)
+  "
+  Update-Changelog -useIssueTitle
+}
+
+function global:au_GetLatest {
+  $release = Get-LatestGithubReleases @repoInfo | % latest
+
+  return @{
+    Version      = $release.Version
+    URL32        = $release.Assets | ? { $_ -match '\.7z$' }
+    ReleaseNotes = $release.Body
+    ReleaseUrl   = $release.ReleaseUrl
+    PackageName  = 'Cmder'
+  }
+}
+
+update -ChecksumFor none
