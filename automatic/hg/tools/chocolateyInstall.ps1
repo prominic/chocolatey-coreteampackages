94ce67d2c863f8b3d528836d9e0de455812328f2
Andrew Hettinger <Andrew Hettinger@prominic-ad.com>
2020-12-14 22:24:48 -0600

    Add hg

---
+++ automatic/hg/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName    = 'hg'
+    installerType  = 'exe'
+    url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-5.6.1-x86-python2.exe'
+    url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-5.6.1-x64-python2.exe'
+    checksum       = 'fe73006ccc384a222e6d5f153a9fea123b6eaab1809196810dfe062a749f89ac'
+    checksumType   = 'sha256'
+    checksum64     = 'b856d70040aab218613c9b36f7f333fe1050c3498e053e103b88773d2bb977f5'
+    checksumType64 = 'sha256'
+    silentArgs     = '/VERYSILENT'
+    validExitCodes = @(0, 3010)
+}
+
+
+Install-ChocolateyPackage @packageArgs
+
+try {
+    $regEntries = @(Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | Where-Object { $_.GetValue('DisplayName') -like 'Mercurial*' })
+    foreach ($regEntry in $regEntries) {
+        $installLocation = $regEntry.GetValue('InstallLocation')
+        if ($installLocation) {
+            Install-ChocolateyPath $installLocation
+        }
+    }
+}
+catch {
+    throw $_
+}
---
+++ automatic/hg/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_list = Invoke-WebRequest -Uri 'https://www.mercurial-scm.org/sources.js';
+
+    $re  = 'https.+/windows/Mercurial-(\d+\.\d+(?:\.\d+)?)(?:-x86|-x64)-python2\.exe';
+    $urls = $download_list.Content.Split("`n") |
+                Where-Object { $_ -match $re } |
+                Select-String -Pattern $re |
+                ForEach-Object { $_.Matches[0].Groups[0].Value };
+
+    $versionMatch = $urls[0] | Select-String -Pattern $re;
+    $version = $versionMatch.Matches[0].Groups[1].Value;
+    $url32 = $urls[0];
+    $url64 = $urls[1];
+
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version };
+    return $Latest;
+}
+
+update
