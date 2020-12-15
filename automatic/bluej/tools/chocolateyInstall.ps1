a3b98f790e0d2db5310f604c8616500c56ec66cd
Andrew Hettinger <Andrew Hettinger@prominic-ad.com>
2020-12-14 22:53:30 -0600

    Add bluej

---
+++ automatic/bluej/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
+  Write-Error -Message "32-bit version of BlueJ not available" -Category ResourceUnavailable
+}
+
+$packageArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  FileType       = 'msi'
+  Url64Bit       = 'http://www.bluej.org/download/files/BlueJ-windows-422.msi'
+  Checksum64     = '6a70e4a9fd48bc7e22143c90ad0bba624b88187dcf70a61893d020fd2b61f033'
+  ChecksumType64 = 'sha256'
+  SilentArgs     = '/quiet /qn /norestart'
+  ValidExitCodes = @(0,3010)
+}
+
+Install-ChocolateyPackage @packageArgs
