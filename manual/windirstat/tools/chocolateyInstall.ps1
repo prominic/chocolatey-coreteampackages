69660bc657781335bc40a9f566ce6e3206b2c553
Andrew Hettinger <Andrew Hettinger@prominic-ad.com>
2020-12-14 23:06:35 -0600

    Add windirstat

---
+++ manual/windirstat/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = 'WinDirStat'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'windirstat1_1_2_setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  softwareName  = 'WinDirStat*'
+  file          = $fileLocation
+  fileType      = 'exe'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  checksum      = '370A27A30EE57247FADDEB1F99A83933247E07C8760A07ED82E451E1CB5E5CDD'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
