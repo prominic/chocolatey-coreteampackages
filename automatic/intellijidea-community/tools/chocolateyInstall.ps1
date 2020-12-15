3dc704d8e364d27bb15279e0b170f6985ada1952
Andrew Hettinger <Andrew Hettinger@prominic-ad.com>
2020-12-14 22:40:26 -0600

    Add intellijidea-community

---
+++ automatic/intellijidea-community/tools/chocolateyInstall.ps1
@@ -0,0 +1,50 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$softwareName = 'IntelliJ IDEA Community Edition*'
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -gt 0) {
+    Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
+}
+
+$url = 'https://download.jetbrains.com/idea/ideaIC-2020.3.exe'
+$sha256sum = '230747bfcd2f431a35e250ca787c9d94f91ce1c7f073bc6e61015c6544188006'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ([System.Environment]::Is64BitOperatingSystem) {
+    $programFiles = $env:ProgramFiles
+}
+else {
+    $programFiles = ${env:ProgramFiles(x86)}
+}
+$installDir = "$programFiles\JetBrains\IntelliJ IDEA Community Edition $env:ChocolateyPackageVersion"
+
+$pp = Get-PackageParameters
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
+
+$silentArgs = "/S /CONFIG=$toolsDir\silent.config "
+$silentArgs += "/D=$installDir"
+
+New-Item -ItemType Directory -Force -Path $installDir
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    url64bit       = $url
+
+    softwareName   = $softwareName
+
+    checksum       = $sha256sum
+    checksumType   = 'sha256'
+    checksum64     = $sha256sum
+    checksumType64 = 'sha256'
+
+    silentArgs     = $silentArgs
+    validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
